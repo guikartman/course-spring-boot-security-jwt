d7224dc5c531cdd6cc8f48c32fb12c1aeec3e411
Guilherme Dantas <guikartman@gmail.com>
2021-02-18 10:58:56 -0300

    Creating the project

---
+++ src/main/java/com/guikartman/CourseSpringBootSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.guikartman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSpringBootSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSpringBootSecurityApplication.class, args);
+	}
+
+}
